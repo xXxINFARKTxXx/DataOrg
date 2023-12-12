ae8a9c6d6fe5184862db12d153a4bb2bc4be6d25
Vladimir Getmantsev <v.getmantsev@yandex.ru>
2023-12-12 20:50:54 +0300

    update

---
+++ seminars/05_python_data/my_solution/exercises_txt/txt_zadatak2.py
@@ -0,0 +1,28 @@
+def readfile(filename: str) -> list:
+    with open(filename, mode='r', encoding='utf') as f:
+        text = f.readlines()
+        tex
+        return text
+
+
+def count_words(text: str) -> int:
+    pass
+
+
+def count_chars(text: str) -> int:
+    pass
+
+
+def count_sentences(text: str) -> int:
+    pass
+
+
+def count_numbers(text: str) -> int:
+    pass
+
+
+def count_letters(text: str) -> int:
+    pass
+
+if __name__ == '__main__':
+    pass
